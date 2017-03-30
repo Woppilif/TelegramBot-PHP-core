52d9b713f3eacac5318dd19a5bf9472626e37624
Woppilif <blink.kerr@gmail.com>
2017-03-30 17:03:30 +0300

    Update config.php

--- engine/config.php
+++ engine/config.php
@@ -1,13 +1,13 @@
 <?php
 $config = array(
-  'token' => '311147139:AAEpmrpjq6W2rOX9lzjJtSESgZbdrvf7cK8',
+  'token' => 'token',
   'api_url' => 'https://api.telegram.org/bot',
-  'webhook_url' => 'https://e4fc0431.ngrok.io/',
+  'webhook_url' => '',
   /*Database*/
   'db_user' => 'root',
   'db_host' => 'localhost',
   'db_name' => 'telegram',
-  'db_password' => 'blink182'
+  'db_password' => ''
 
 );
 
